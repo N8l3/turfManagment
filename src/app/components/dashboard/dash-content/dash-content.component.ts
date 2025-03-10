581f112ce5cf84464efe9047dbbd64bda395dc12
Nilesh <ILOM32406@icicilombard.com>
2025-03-11 01:05:00 +0530

    design 2 -fix

--- src/app/components/dashboard/dash-content/dash-content.component.ts
+++ src/app/components/dashboard/dash-content/dash-content.component.ts
@@ -10,15 +10,41 @@ import { CommonModule } from '@angular/common';
 })
 export class DashContentComponent {
   turfs = [
-    { name: 'Andheri Sports Turf', location: 'Andheri, Mumbai', image: 'assets/turf1.jpg' },
-    { name: 'Bandra Football Arena', location: 'Bandra, Mumbai', image: 'assets/turf2.jpg' },
-    { name: 'Worli Kick-Off Turf', location: 'Worli, Mumbai', image: 'assets/turf3.jpg' },
-    { name: 'Dadar Elite Turf', location: 'Dadar, Mumbai', image: 'assets/turf4.jpg' },
-    { name: 'Juhu Premium Turf', location: 'Juhu, Mumbai', image: 'assets/turf5.jpg' },
+    {
+      name: 'Andheri Sports Turf',
+      location: 'Andheri, Mumbai',
+      image: 'assets/turf1.jpg',
+    },
+    {
+      name: 'Bandra Football Arena',
+      location: 'Bandra, Mumbai',
+      image: 'assets/turf2.jpg',
+    },
+    {
+      name: 'Worli Kick-Off Turf',
+      location: 'Worli, Mumbai',
+      image: 'assets/turf3.jpg',
+    },
+    {
+      name: 'Dadar Elite Turf',
+      location: 'Dadar, Mumbai',
+      image: 'assets/turf4.jpg',
+    },
+    {
+      name: 'Juhu Premium Turf',
+      location: 'Juhu, Mumbai',
+      image: 'assets/turf5.jpg',
+    },
+    {
+      name: 'Thane Sports Hub',
+      location: 'Thane, Mumbai',
+      image: 'assets/turf6.jpg',
+    },
   ];
 
   timeSlots = this.generateTimeSlots();
-  bookings: { [key: string]: boolean } = {};
+  bookings: { [key: string]: { booked: boolean; user?: string } } = {};
+  selectedSlot: { turf: string; time: string } | null = null;
 
   generateTimeSlots() {
     const slots = [];
@@ -36,10 +62,18 @@ export class DashContentComponent {
 
   toggleSlot(turfName: string, timeSlot: string) {
     const key = `${turfName}-${timeSlot}`;
-    this.bookings[key] = !this.bookings[key];
+    if (this.bookings[key]?.booked) {
+      delete this.bookings[key];
+    } else {
+      this.selectedSlot = { turf: turfName, time: timeSlot };
+    }
   }
 
-  isBooked(turfName: string, timeSlot: string): boolean {
-    return this.bookings[`${turfName}-${timeSlot}`] || false;
+  confirmBooking() {
+    if (this.selectedSlot) {
+      const key = `${this.selectedSlot.turf}-${this.selectedSlot.time}`;
+      this.bookings[key] = { booked: true, user: 'You' };
+      this.selectedSlot = null;
+    }
   }
 }
